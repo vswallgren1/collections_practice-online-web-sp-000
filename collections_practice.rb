567e608a02550ca7a67e328103ab0c108096d15c
Vince W <vincewallgren@me.com>
2021-02-25 02:43:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+#Question #1
 
+def sort_array_asc(array)
+  array.sort
+end
+
+#Question #2
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end 
+
+#Question #3
+#Question #4
+#Question #5
+#Question #6
+#Question #7
+#Question #8
+#Question #9

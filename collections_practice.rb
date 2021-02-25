ea455d63facfd2845c3c3b0a4f622ce65f853632
Vince W <vincewallgren@me.com>
2021-02-25 02:50:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,6 +21,12 @@ def sort_array_char_count(array)
 end
 
 #Question #4
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
 #Question #5
 #Question #6
 #Question #7
